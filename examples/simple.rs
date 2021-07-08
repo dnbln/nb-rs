0e0c494b476d6bf1493a20edd703346ae6f7f97a
Dinu Blanovschi <dinu.blanovschi@criptext.com>
2021-07-08 17:09:27 +0300

    Update example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,6 @@
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let img_url: String = nekosbest::get(nekosbest::Category::Nekos).await?;
+    let img_url: String = nekosbest::get(nekosbest::Category::Pat).await?;
     println!("{}", img_url);
     Ok(())
 }
